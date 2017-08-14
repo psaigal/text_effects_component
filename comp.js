45c85636dc0c63353483f6324870792d27bae5da
psaigal <psaigal48@gmail.com>
2017-08-14 16:16:06 -0700

    Add form and basic css

---
+++ comp.js
@@ -0,0 +1,13 @@
+var newComponent = (function(){
+	'use strict';
+
+	//When user submits text into text box and hits submit, first a new TextEffect object is created.
+	//The appropriate function will be called depending on what button was clicked
+
+	function TextEffects(text) {
+		this.text = text;
+	}
+
+	return {
+	}
+})();
