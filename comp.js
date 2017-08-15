78ac362025d89fd3674e1481d87195c0d4130ab1
psaigal <psaigal48@gmail.com>
2017-08-15 15:08:30 -0700

    Add styling and fix bug in piglatin function

--- comp.js
+++ comp.js
@@ -1,68 +1,79 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-	var newComponent = (function(){
-		//private functions should have _ in the name
+var newComponent = (function(){
+ 	document.addEventListener("DOMContentLoaded", function(event) { 
+
 		'use strict';
 
-		//public variables 
-		var originalUserText = document.getElementById("input-text").value;
-		console.log(originalUserText);
-		var originalUserTextLength = originalUserText.length;
+		//CACHE DOM
+		var originalUserText = document.getElementById("input-text");
 		var pgButton = document.getElementById("pig-latin");
 		var reverseButton = document.getElementById("reverse");
+		var clearButton = document.getElementById("clear");
 		var resultContainer = document.getElementById("result");
 
-		pgButton.addEventListener("click", pigLatinCb);
 
+		//BIND EVENTS
+		pgButton.addEventListener("click", pigLatinCb);
+		reverseButton.addEventListener("click", reverseStringCb);
+		clearButton.addEventListener("click", clearButtonCb);
+
+
+		//CALLBACKS
 		function pigLatinCb(event) {
 			event.preventDefault();
-			originalUserText = document.getElementById("input-text").value;
-			if (typeof originalUserText === "string" && originalUserText.length > 0) {
-				pigLatin(originalUserText);
+			if (typeof originalUserText.value === "string" && originalUserText.value.length > 0) {
+				return _result(pigLatin(originalUserText.value));
 			}
 		}
 
+		function reverseStringCb(event) {
+			event.preventDefault();
+			if (typeof originalUserText.value === "string" && originalUserText.value.length > 0) {
+				return _result(reverseString(originalUserText.value));
+			}
+		}
+
+		function clearButtonCb() {
+			resultContainer.innerHTML = "";
+			originalUserText.value = "";
+		}
+
+		//HELPER FUNCTIONS
+		function _isVowel(char) {
+			if (char.match(/[a-z]/i)) {
+				char = char.toLowerCase();
+			  	return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || char === "y" || false;
+			}
+		}
+
+
+		//PUBLIC API FUNCTIONS
 		function pigLatin(text) {
 			var textArr = text.split(" ");
 		  	for(var i = 0; i <= textArr.length-1; i++) {
-				if (isVowel(textArr[i][0])) {
-					textArr[i] = textArr[i] + "yay";
-				}
-				else {
-					var suffix = "";
-					var slice = 0;
-					for (var y = 0; y <= (textArr[i].length)-1; y++) {
-						if (!isVowel(textArr[i][y])) {
-						  suffix += textArr[i].slice(y,y+1);
-						  slice += 1;
-						}
-						else {
-						  textArr[i] = textArr[i].slice(slice, textArr[i].length) + suffix + "ay";
-						  break;
+		  		if(textArr[i].match(/[a-z]/i )){
+					if (_isVowel(textArr[i][0])) {
+						textArr[i] = textArr[i] + "yay";
+					}
+					else {
+						var suffix = "";
+						var slice = 0;
+						for (var y = 0; y <= (textArr[i].length)-1; y++) {
+							if (!_isVowel(textArr[i][y])) {
+							  suffix += textArr[i].slice(y,y+1);
+							  slice += 1;
+							}
+							else {
+							  textArr[i] = textArr[i].slice(slice, textArr[i].length) + suffix + "ay";
+							  break;
+							}
 						}
 					}
 				}
 			}
-			return result(textArr.join(" "));
+			return textArr.join(" ");
 		}
 
-		function isVowel(char) {
-			char = char.toLowerCase();
-		  	return char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' || char === "y" || false;
-		}
-
-
-		reverseButton.addEventListener("click", reverseStringCb);
-
-		function reverseStringCb(event) {
-			event.preventDefault();
-			originalUserText = document.getElementById("input-text").value;
-			console.log(typeof originalUserText === "string");
-			console.log(originalUserText);
-			if (typeof originalUserText === "string" && originalUserText.length > 0) {
-				reverseString(originalUserText);
-			}
-		}
-
+		
 		function reverseString(text) {
 		  if (text.length > 0 && typeof text === "string") {
 		    var textArr = text.split(" ");
@@ -73,22 +84,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		        newStr += textArr[i].substring(strLength-1,strLength);
 		        strLength--;
 		      }
-		      console.log(newStr);
 		      textArr[i] = newStr;
 		    }
 		  }
-		  return result(textArr.join(" "));
+		  return textArr.join(" ");
 		}
 
-		function result(newText) {
+
+		//RENDER
+		function _result(newText) {
 			resultContainer.innerHTML = newText;
 		}
-		// return {
-		// 	setPigLatin: pigLatin,
-		// 	setReverse: reverseString
-		// }
-	})();
-});
+
+
+		//PUBLIC API
+		return {
+			setPigLatin: pigLatin,
+			setReverse: reverseString
+		}
+	});
+})();
+
 
 
 										
