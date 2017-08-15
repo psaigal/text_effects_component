46e7a1f3039a8147ec6ba7ce4b533334f7abb96b
psaigal <psaigal48@gmail.com>
2017-08-15 11:56:44 -0700

    Add reverse function and event listeners

--- comp.js
+++ comp.js
@@ -1,13 +1,71 @@
-var newComponent = (function(){
-	'use strict';
+document.addEventListener("DOMContentLoaded", function(event) { 
+	var newComponent = (function(){
+		//private functions should have _ in the name
+		'use strict';
 
-	//When user submits text into text box and hits submit, first a new TextEffect object is created.
-	//The appropriate function will be called depending on what button was clicked
+		//public variables 
+		var originalUserText = document.getElementById("input-text").value;
+		console.log(originalUserText);
+		var originalUserTextLength = originalUserText.length;
+		var pgButton = document.getElementById("pig-latin");
+		var reverseButton = document.getElementById("reverse");
+		var resultContainer = document.getElementById("result");
 
-	function TextEffects(text) {
-		this.text = text;
-	}
+		pgButton.addEventListener("click", pigLatinCb);
 
-	return {
-	}
-})();
+		function pigLatinCb(event) {
+			event.preventDefault();
+			if (typeof originalUserText === "string" && originalUserTextLength > 0) {
+				pigLatin(originalUserText);
+			}
+		}
+
+		function pigLatin(text) {
+			var lastLetter = text.slice(0,1);
+			var text = text.slice(1,text.length) + lastLetter + "a";
+			return text;
+		}
+
+		reverseButton.addEventListener("click", reverseStringCb);
+
+		function reverseStringCb(event) {
+			// event.preventDefault();
+			originalUserText = document.getElementById("input-text").value;
+			console.log(typeof originalUserText === "string");
+			console.log(originalUserText);
+			if (typeof originalUserText === "string" && originalUserText.length > 0) {
+				console.log("vghvchvh");
+				reverseString(originalUserText);
+			}
+		}
+
+		function reverseString(text) {
+		console.log("vhgvj");
+		  if (text.length > 0 && typeof text === "string") {
+		    var textArr = text.split(" ");
+		    for (var i = 0; i <= textArr.length-1; i++) {
+		      var newStr = "";
+		      var strLength = textArr[i].length;
+		      while (strLength > 0) {
+		        newStr += textArr[i].substring(strLength-1,strLength);
+		        strLength--;
+		      }
+		      console.log(newStr);
+		      textArr[i] = newStr;
+		    }
+		  }
+		  return result(textArr.join(" "));
+		}
+
+		function result(newText) {
+			resultContainer.innerHTML = newText;
+		}
+		// return {
+		// 	setPigLatin: pigLatin,
+		// 	setReverse: reverseString
+		// }
+	})();
+});
+
+
+										
